8c39069a73b7ed9d353d6abc63f463fa1cb8c6fd
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-08-11 23:25:40 -0400

    Update Impl Nested Patterns

--- NestedPattern.java
+++ NestedPattern.java
@@ -1,23 +1,32 @@
-/**
- * The `NestedPattern` class in Java takes an input number and prints a square pattern of asterisks
- * with the specified number of rows and columns.
- */
 import java.util.Scanner;
 
 public class NestedPattern 
 {
-    /**
-     * @param args The `args` parameter in the `main` method is an array of strings that allows you to
-     * pass command line arguments when running the Java program. These arguments can be accessed
-     * within the program if needed.
-     */
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number of Rows / Columns");
+        System.out.println();
+        System.out.print("Enter the Number of Rows / Columns: ");
         
         int n = sc.nextInt();
 
+        System.out.println();
+        System.out.println("Nested Pattern");
+        NestedPattern np = new NestedPattern();
+        np.nestPattern(n);
+
+        System.out.println();
+
+        System.out.println("Right-Angled Triangle Pattern");
+        np.RightAngPattern(n);
+        
+        System.out.println();
+
+        sc.close();
+    }
+
+    public void nestPattern(int n)
+    {
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n; j++)
@@ -26,6 +35,17 @@ public class NestedPattern
             }
             System.out.println();
         }
-        sc.close();
+    }
+
+    public void RightAngPattern(int n)
+    {
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
