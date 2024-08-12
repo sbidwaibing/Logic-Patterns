3b2ffaea344853ff6b0341537c7f427d8ce8e214
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-08-12 19:46:35 -0400

    Updated NestedPattern.java

--- NestedPattern.java
+++ NestedPattern.java
@@ -16,9 +16,16 @@ public class NestedPattern
         np.nestPattern(n);
 
         System.out.println();
-
         System.out.println("Right-Angled Triangle Pattern");
         np.RightAngPattern(n);
+
+        System.out.println();
+        System.out.println("Right-Angled Number Triangle Pattern");
+        np.RightAngNumbPattern(n);
+
+        System.out.println();
+        System.out.println("Right-Angled Number 2 Triangle Pattern");
+        np.RightAngNumb2Pattern(n);
         
         System.out.println();
 
@@ -48,4 +55,60 @@ public class NestedPattern
             System.out.println();
         }
     }
+
+    public void RightAngNumbPattern(int n)
+    {
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void RightAngNumb2Pattern(int n)
+    {
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+//OUTPUT: 
+//Enter the Number of Rows / Columns: 5
+
+// Nested Pattern
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// Right-Angled Triangle Pattern
+// *
+// **
+// ***
+// ****
+// *****
+
+// Right-Angled Number Triangle Pattern
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+
+// Right-Angled Number 2 Triangle Pattern
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
